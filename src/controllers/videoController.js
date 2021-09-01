b9c2cce2a3951648c29ac95f8e7f652cae49a295
alpaca <zxcvbnm5288@gmail.com>
2021-09-01 23:36:31 +0900

    learn pug mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,33 @@
-const fakeUser = {
-  username: "alpaca",
-  loggedIn: false,
-};
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: 'Hello',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes ago',
+      views: 59,
+      id: 1
+    },
+    {
+      title: 'Hola',
+      rating: 15,
+      comments: 32,
+      createdAt: '277 minutes ago',
+      views: 159,
+      id: 2
+    },
+    {
+      title: '안녕',
+      rating: 51,
+      comments: 22,
+      createdAt: '23 minutes ago',
+      views: 591,
+      id: 3
+    },
+  ];
 
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "home", fakeUser });
+  return res.render("home", { pageTitle: "home", videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
