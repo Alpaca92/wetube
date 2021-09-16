e2249884fc86a4e56ea707db9939efd137a51eb2
alpaca <zxcvbnm5288@gmail.com>
2021-09-16 13:33:11 +0900

    learn how to save password using bcrypt

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,29 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
+  return res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = async (req, res) => {
+  const { email, password, name, username, location } = req.body;
+
+  try {
+    await User.create({
+      email,
+      password,
+      name,
+      username,
+      location
+    });
+  
+    return res.redirect("/login")
+  } catch(error) {
+    return res.render("join", { pageTitle: "Join" });
+  }
+};
+
 export const edit = (req, res) => res.send("edit");
 export const remove = (req, res) => res.send("remove");
 export const login = (req, res) => res.send("login");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see");
-
