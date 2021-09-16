a1b60e74f99338b4ab00576c22338be5084e0373
alpaca <zxcvbnm5288@gmail.com>
2021-09-17 04:04:48 +0900

    learn how to create login authentication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,25 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
 };
 
 export const postJoin = async (req, res) => {
-  const { email, password, name, username, location } = req.body;
-  const exists = await User.exists(({ $or: [{ username }, { email }] }));
+  const { email, password, confirmPassword, name, username, location } =
+    req.body;
+
+  if (password !== confirmPassword) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "password가 일치하지 않습니다.",
+    });
+  }
+
+  const exists = await User.exists({ $or: [{ username }, { email }] });
 
   if (exists) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle: "Join",
       errorMessage: "이미 username(or email)이 존재합니다.",
     });
@@ -24,14 +34,41 @@ export const postJoin = async (req, res) => {
       location,
     });
 
-    return res.redirect("/login");
+    return res.redirect("/join");
   } catch (error) {
-    return res.render("join", { pageTitle: "Join" });
+    return res
+      .status(400)
+      .render("join", { pageTitle: "Join", errorMessage: error._message });
   }
+};
+
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: `Login` });
+};
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle: `Login`,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+
+  const matchPassword = await bcrypt.compare(password, user.password);
+  if (!matchPassword) {
+    return res.status(400).render("login", {
+      pageTitle: `Login`,
+      errorMessage: "wrong password.",
+    });
+  }
+
+  return res.end();
 };
 
 export const edit = (req, res) => res.send("edit");
 export const remove = (req, res) => res.send("remove");
-export const login = (req, res) => res.send("login");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see");
