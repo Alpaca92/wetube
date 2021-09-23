e4e3ceb00b52b0c3300636842b4f16b9fd0ef0d7
alpaca <zxcvbnm5288@gmail.com>
2021-09-23 10:12:44 +0900

    how to check session store

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,6 +13,20 @@ app.set("view engine", "pug");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "hello",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((err, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
