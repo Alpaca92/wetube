8ed47826baab0803ba5f43e73f09eb8fd348be55
alpaca <zxcvbnm5288@gmail.com>
2022-01-07 17:08:00 +0900

    #4.0 - #4.1: create router

--- src/server.js
+++ src/server.js
@@ -5,6 +5,26 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.get("/", (req, res) => res.send(`<h1>This is main page</h1>`));
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("edit user");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("watch video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 app.listen(5000, () => console.log(`http://localhost:5000`));
