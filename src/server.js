990fef220ebd9d1faf120c985be250c05972f8d0
alpaca <zxcvbnm5288@gmail.com>
2022-01-10 12:56:52 +0900

    #4.2 - #4.5: clean code

--- src/server.js
+++ src/server.js
@@ -1,27 +1,12 @@
 import express from "express";
 import logger from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
 app.use(logger("dev"));
-
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("edit user");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("watch video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
