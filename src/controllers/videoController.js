6500b4879212a311f2aa1352e86c3ab22969df1a
alpaca <zxcvbnm5288@gmail.com>
2022-01-12 22:08:06 +0900

    #5.7 - #6.0: understand conditionals, iteration and mixins of pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,40 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+let videos = [
+  {
+    title: "1st video",
+    rating: 5,
+    comments: 3,
+    createdAt: "2 minutes ago",
+    views: 593,
+    id: 0,
+  },
+  {
+    title: "2nd video",
+    rating: 5,
+    comments: 3,
+    createdAt: "412 minutes ago",
+    views: 1359,
+    id: 1,
+  },
+  {
+    title: "3rd video",
+    rating: 5,
+    comments: 3,
+    createdAt: "21 minutes ago",
+    views: 51239,
+    id: 2,
+  },
+];
 
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id];
+
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 
 export const edit = (req, res) => res.send("edit");
 
