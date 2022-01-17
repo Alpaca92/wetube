62194239c55c9675273e06977a1fadc769cc7553
alpaca <zxcvbnm5288@gmail.com>
2022-01-17 14:50:33 +0900

    #6.14 - #6.17: validate data use mongoose schema

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,6 +30,21 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: `Upload Video` });
 };
 
-export const postUpload = (req, res) => {
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(","),
+    });
+
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: `Upload Video`,
+      errorMessage: error._message,
+    });
+  }
 };
