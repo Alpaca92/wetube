adc0eefff96cb9e67b4e49682ed42cab16c95896
alpaca <zxcvbnm5288@gmail.com>
2022-01-19 18:09:49 +0900

    #7.0 - #7.2: create user with bcrypt

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,11 +1,29 @@
-export const join = (req, res) => res.send(`join`);
+import User from "../models/User";
 
-export const edit = (req, res) => res.send("edit");
+export const getJoin = (req, res) =>
+  res.render("join", { pageTitle: "Create Account" });
 
-export const remove = (req, res) => res.send("remove");
+export const postJoin = async (req, res) => {
+  const { email, username, password, name, location } = req.body;
+
+  await User.create({
+    email,
+    username,
+    password,
+    name,
+    location,
+  });
+
+  res.redirect("/login");
+};
+
 
 export const login = (req, res) => res.send("login");
 
 export const logout = (req, res) => res.send("logout");
+
+export const edit = (req, res) => res.send("edit");
+
+export const remove = (req, res) => res.send("remove");
 
 export const see = (req, res) => res.send(`${req.params.id} see`);
--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 import "./db";
 import "./models/Video";
+import "./models/User";
 import app from "./server";
 
 app.listen(5000, () => console.log(`http://localhost:5000`));
