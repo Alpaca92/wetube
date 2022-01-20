9662c0f004fbe361c1b18fc5a0a5b6e4325b4df6
alpaca <zxcvbnm5288@gmail.com>
2022-01-20 14:40:33 +0900

    #7.7 - #7.11: express-session

---
+++ src/middleware/localMiddleware.js
@@ -0,0 +1,9 @@
+export const localsMiddleware = (req, res, next) => {
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.loggedInUser = req.session.user;
+  res.locals.siteName = "Wetube";
+
+  console.log(req.session, req.session.id);
+
+  next();
+};
--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 import express from "express";
+import session from "express-session";
 import logger from "morgan";
+import { localsMiddleware } from "./middleware/localMiddleware";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
-app.use(logger("dev"));
-
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
+app.use(logger("dev"));
+app.use(
+  session({
+    secret: "secret string",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(localsMiddleware);
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
