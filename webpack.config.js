d6dfe1061f46cc3a21540773ca62851ba03ec4f3
alpaca <zxcvbnm5288@gmail.com>
2022-01-27 15:41:29 +0900

    #9.4 - #9.7: use clean property in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   mode: "development",
   entry: "./src/client/js/main.js",
   output: {
-    path: path.resolve(__dirname, "assets", "js"),
-    filename: "main.js",
+    clean: true,
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
   },
   watch: true,
   watchOptions: {
-    aggregateTimeout: 3000,
-    poll: 3000,
+    aggregateTimeout: 1000,
+    poll: 2000,
   },
   module: {
     rules: [
@@ -23,6 +30,10 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
